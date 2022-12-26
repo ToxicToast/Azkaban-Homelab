c26971f13c958742e80fe3938c4573d41405d840
Thomas Kortyka <t.kortyka@gmx.de>
2022-12-26 02:06:52 +0100

    Add Shared Folder

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/enums';
+export * from './lib/functions';
+export * from './lib/interfaces';
+export * from './lib/models';
+export * from './lib/modules';
+export * from './lib/types';
